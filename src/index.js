b286cd27da6e8bc1ca22064602806e7b09193d6b
Dzmitry Karachun <dzmitry.karachun@bk.ru>
2018-09-30 21:22:08 +0300

    __

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 != 0) return false;
-    var arr = [];
-    for ( var i = 0; i < str.length; i++)
-        for ( var j = 0; j < bracketsConfig.length; j++) {
+    let arr = [];
+    for ( let i = 0; i < str.length; i++)
+        for ( let j = 0,len = bracketsConfig.length; j < len; j++) {
             if (str[i] == bracketsConfig[j][1] && arr[arr.length - 1] == bracketsConfig[j][0])
                 arr.pop();
             else if (str[i] == bracketsConfig[j][0])
